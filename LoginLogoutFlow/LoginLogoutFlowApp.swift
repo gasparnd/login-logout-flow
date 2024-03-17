417116825d19b093cd4939e9ecc601caee3c4874
gasparnd <gaspardolcemascolo@gmail.com>
2024-03-16 22:42:58 -0300

    Initial Commit

---
+++ LoginLogoutFlow/LoginLogoutFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginLogoutFlowApp.swift
+//  LoginLogoutFlow
+//
+//  Created by Gaspar Dolcemascolo on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginLogoutFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
